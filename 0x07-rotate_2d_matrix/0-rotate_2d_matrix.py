f475bbca2221af0e307baadfb49030246211d3bc
Irvine Sunday <amugumbiirvine@gmail.com>
2023-08-16 18:35:08 +0000

    modified 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,14 +11,15 @@ def rotate_2d_matrix(matrix):
 
     while l > 0:
         new_list = []
-        y = n
-        while y > 0:
+        y = n - 1
+        while y >= 0:
             new_list.append(matrix[y][x])
             y -= 1
 
-        rotated.append(new_list)
+        rotated_matrix.append(new_list)
+        print(new_list)
         l -= 1
         x += 1
-
+    print(rotated_matrix)
     return rotated_matrix
 
